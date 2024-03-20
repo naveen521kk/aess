980b347c7e95bc2ae5807985082c0c7b0e9e2481
Pranav Raj H <vinstarpranav@gmail.com>
2024-03-20 12:51:14 +0530

    Delete unnecessary database and dependency files, and update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,39 +1,9 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Float, Boolean, TIMESTAMP
-from sqlalchemy.orm import relationship
+import mongoengine as db
 
-from .database import Base
+db.connect('essay_scores')
 
-
-class Submission(Base):
-    __tablename__ = "submissions"
-
-    userid = Column(String(128) , primary_key=True)
-    essayid = Column(String(128), primary_key=True)
-
-
-class Score(Base):
-    __tablename__ = "scores"
-
-    userid = Column(String(128), ForeignKey("submissions.userid", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
-    essayid = Column(String(128), ForeignKey("submissions.essayid", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
-    feedback = Column(String(5120), default=None)
-    suggestion = Column(String(5120), default=None)
-    score = Column(Integer, default=None)
-
-
-class RubricScore(Base):
-    __tablename__ = "rubric_scores"
-
-    rubricid = Column(Integer, ForeignKey("rubrics.rubricid", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
-    userid = Column(String(128), ForeignKey("submissions.userid", ondelete="CASCADE", onupdate="CASCADE"))
-    essayid = Column(String(128), ForeignKey("submissions.essayid", ondelete="CASCADE", onupdate="CASCADE"), primary_key=True)
-    score = Column(Integer, default=None)
-    feedback = Column(String(5120), default=None)
-
-
-class Rubric(Base):
-    __tablename__ = "rubrics"
-
-    rubricid = Column(Integer, primary_key=True)
-    score = Column(Integer, default=None)
-    description = Column(String(5120), default=None)
+class Response(db.Document):
+    userid = db.StringField(required=True)
+    essay = db.StringField(required=True)
+    response = db.DictField()
+    grammar = db.ListField()
